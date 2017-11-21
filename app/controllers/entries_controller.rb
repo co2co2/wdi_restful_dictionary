ac73d4a3d6c7e6abc234e08251c9a542bc03fcf0
co2co2 <coco8287@hotmail.com>
2017-11-21 17:19:23 -0500

    dictionary part 1 done

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,29 @@
+class EntriesController < ApplicationController
+  def index
+    render :index
+  end
+
+  def show
+    render :show
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    redirect_to entries_url
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+    redirect_to entry_url(params[:id])
+  end
+
+  def destroy
+    redirect_to index_url
+  end 
+end
